288f5e604cb842033e504b972df9c24fce37d31e
Emancc <e.cepedacaceres@itecriocuarto.org.ar>
2026-06-25 00:30:03 -0300

    cambios minimos

--- Users/forms.py
+++ Users/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import UsuarioPersonalizado
 
-input_styles = "w-full px-4 py-3 bg-gray-200 border border-gray-200 rounded-xl text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent transition duration-200"
-
 
 class UsuarioPersonalizadoForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -12,21 +10,10 @@ class UsuarioPersonalizadoForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class": input_styles})
-        self.fields["password1"].widget.attrs.update({"class": input_styles})
-        self.fields["password2"].widget.attrs.update({"class": input_styles})
-        self.fields["telefono"].widget.attrs.update({"class": input_styles})
-        self.fields["dni"].widget.attrs.update({"class": input_styles})
-        self.fields["username"].help_text = ""
-        self.fields["password1"].help_text = ""
+        self.fields["password1"].help_text = "minimo 8 caracteres"
 
 
 class UsuarioAuthenticationForm(AuthenticationForm):
     class Meta:
         model = UsuarioPersonalizado
         fields = ("username", "password")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class": input_styles})
-        self.fields["password"].widget.attrs.update({"class": input_styles})
